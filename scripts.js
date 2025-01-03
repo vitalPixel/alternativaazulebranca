2d1cb793399ff063c832a673c0e225867697f8cd
vitalPixel <19647305+vitalPixel@users.noreply.github.com>
2025-01-03 10:45:34 +0000

    moved all of the newsletter fields inside of the modal

--- scripts.js
+++ scripts.js
@@ -1,42 +1,17 @@
 const newsModal = document.getElementById('newsModal');
-const socioInput = document.getElementById('socioInput');
-const emailInput = document.getElementById('email');
-
-function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
 
 function openModal() {
-    emailValue = emailInput.value;
-    if (!emailValue) {
-        alert('Preenche o campo de email, por favor');
-        return;
-    } else if (!validateEmail(emailValue)) {
-        alert('Email inválido');
-        return;
-    }
-    console.log(emailValue)
-    newsModal.style.display = "block";
+    newsModal.style.display = "flex";
 }
 
-function updateSocioValue(value) {
-    socioInput.value = value;
-    console.log(socioInput.value);
-}
-
-function submitForm() {
-    const form = document.querySelector('.newsletter__form');
-    const socioValue = document.getElementById('socioInput').value;
-
-    if (!socioValue) {
-        alert('Seleciona uma opção, por favor');
-        return;
-    }
-
-    console.log(socioInput.value);
-    console.log(emailInput.value);
-    form.submit();
+function closeModal() {
     newsModal.style.display = "none";
-    form.reset();
 }
+
+document.addEventListener('click', function(event) {
+    const closeNewsModal = document.querySelector('.newsletter__modal');
+
+    if (event.target === closeNewsModal) {
+        closeModal();
+    }
+});
